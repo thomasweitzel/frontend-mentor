620838bc9fb8213c265eec5b2f4d37d180402db0
Thomas Weitzel <tweitzel@synformation.com>
2024-11-26 18:15:56 +0100

    update the project to use flex for centering the component

--- qr-code-component/src/components/card.jsx
+++ qr-code-component/src/components/card.jsx
@@ -2,15 +2,17 @@ const Card = ({ info }) => {
   const { title, text, image, imageAlt } = info;
 
   return (
-    <article className={"absolute left-1/2 top-1/2 h-card w-card -translate-x-1/2 -translate-y-1/2 rounded-2xl border bg-white shadow-lg"} aria-labelledby={"card-title"}>
-      <div className={"m-4 h-image w-image"}>
-        <img className={"rounded-lg object-cover"} src={image} alt={imageAlt} />
-      </div>
-      <h1 className={"px-8 py-2 text-center text-card-big font-bold leading-tight text-gray-900"} id={"card-title"}>
-        {title}
-      </h1>
-      <p className={"px-8 py-2 text-center text-card-small leading-snug text-gray-500"}>{text}</p>
-    </article>
+    <div className={"flex items-center justify-center h-screen"}>
+      <article className={"h-card w-card rounded-2xl border bg-white shadow-lg"} aria-labelledby={"card-title"}>
+        <div className={"m-4 h-image w-image"}>
+          <img className={"rounded-lg object-cover"} src={image} alt={imageAlt} />
+        </div>
+        <h1 className={"px-8 py-2 text-center text-card-big font-bold leading-tight text-gray-900"} id={"card-title"}>
+          {title}
+        </h1>
+        <p className={"px-8 py-2 text-center text-card-small leading-snug text-gray-500"}>{text}</p>
+      </article>
+    </div>
   );
 };
 
